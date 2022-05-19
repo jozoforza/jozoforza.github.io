220cc718d1c25eb4adb005a246e3a9980ce935fb
jozoforza <s.macica7@gmail.com>
2022-05-19 15:02:34 +0200

    vsetky checkboxy

--- checkbox.js
+++ checkbox.js
@@ -28,44 +28,30 @@ const specialne ={
   linky: [],
   nazvy: []
 }
+//5
+const monocot={
+  linky: ['1-klicne\\Šafran bielokvetý.JPG',
+          '1-klicne\\Hyacint východný.JPG'],
+  nazvy: ['Šafran bielokvetý','Hyacint východný']
+}
+//6
+const dicot={
+  linky: ['2-klicne\\Bocianik rozpukovitý.JPG',
+          '2-klicne\\Deväťsil biely.JPG',
+          '2-klicne\\Fialka voňavá.JPG'],
+  nazvy: ['Bocianik rozpukovitý','Deväťsil biely','Fialka voňavá']
+}
 
-objekty=[huby, riasy, machy, paprade, nahe];
-
-
-//LINKY
-//const huby=["huby\\images (1).jpg"]
-
-/*const riasy=["riasy\\cervena.jpg",
-            "riasy\\hneda.jpg",
-            "riasy\\modra.jpg"]*/
-
-const monocot=['1-klicne\\Šafran bielokvetý.JPG',
-               '1-klicne\\Hyacint východný.JPG']
-
-const dicot=['2-klicne\\Bocianik rozpukovitý.JPG',
-             '2-klicne\\Deväťsil biely.JPG',
-             '2-klicne\\Fialka voňavá.JPG']
-
-//NAZVY
-//const hubyNazvy=["cervenaHuba"]
-const riasyNazvy=["cervenaRiasa","hnedaRiasa","modraRiasa"]
-const monocotNazvy=['Šafran bielokvetý','Hyacint východný']
-const dicotNazvy=['Bocianik rozpukovitý','Deväťsil biely','Fialka voňavá']
-
-//CHECKBOXY
-//var checkBoxHuby = document.getElementById("hubyBox");
-//var checkBoxRiasy = document.getElementById("riasyBox");
-var checkboxMonocot = document.getElementById("monocotBox");
-var checkboxDicot = document.getElementById("dicotBox");
-
+objekty=[huby, riasy, machy, paprade, nahe, monocot, dicot, specialne];
 const galeria= document.getElementById("galeria");
 
 //VYPIS OBRAZKOV
-/*for(i=0;i<objekty.length;i++){
-    for(i=0;i<objekty[i].linky.length;i++){
+/*for(i=0;i<8;i++){
+    let objekt=objekty[i]
+    for(a=0;a<objekt.linky.length;a++){
       galeria.innerHTML+=`
-      <div id="${objekty[i].nazvy[i]}" class="galeryBox" >
-          <img class="galeryImage" src="${objekty[i].linky[i]}" >   
+      <div id="${objekt.nazvy[i]}" class="galeryBox" >
+          <img class="galeryImage" src="${objekt.linky[i]}" >   
       </div>
       `}
     }*/
@@ -92,38 +78,20 @@ for(i=0;i<machy.linky.length;i++){
   </div>
   `}
 
-for(i=0;i<monocot.length;i++){
+for(i=0;i<monocot.linky.length;i++){
   galeria.innerHTML+=`
-  <div id="${monocotNazvy[i]}" class="galeryBox" >
-      <img class="galeryImage" src="${monocot[i]}" >   
+  <div id="${monocot.nazvy[i]}" class="galeryBox" >
+      <img class="galeryImage" src="${monocot.linky[i]}" >   
   </div>
   `}
 
-for(i=0;i<dicot.length;i++){
+for(i=0;i<dicot.linky.length;i++){
 galeria.innerHTML+=`
-<div id="${dicotNazvy[i]}" class="galeryBox" >
-    <img class="galeryImage" src="${dicot[i]}" >   
+<div id="${dicot.nazvy[i]}" class="galeryBox" >
+    <img class="galeryImage" src="${dicot.linky[i]}" >   
 </div>
 `}
 
-//CHECKBOX FUNKCIE
-    function hubyFunction() {
-      alert("pupek")
-        // Get the checkbox
-        // Get the output text
-        // If the checkbox is checked, display the output text
-        /*if (document.getElementById(idName).checked == true){
-          //galeria.style.display = "grid";
-          for(i=0;i<huby.linky.length;i++){
-              document.getElementById(huby.nazvy[i]).style.display = "block";
-          }
-        } else {
-          //galeria.style.display = "none";
-          for(i=0;i<huby.linky.length;i++){
-            document.getElementById(huby.nazvy[i]).style.display = "none";
-        }
-        }*/
-      }
 
       function hubyF(identification){
         //alert(typeof identification);
@@ -131,70 +99,14 @@ galeria.innerHTML+=`
         //alert(medziobjekt+1)
         //alert(identification)
         if (document.getElementById(identification).checked == true){
-          alert("identification")
+          //alert("identification")
           for(i=0;i<objekty[medziobjekt].linky.length;i++)
           document.getElementById(objekty[medziobjekt].nazvy[i]).style.display = "block";
         }
         else{
-          alert("not checked")
+          //alert("not checked")
           for(i=0;i<objekty[medziobjekt].linky.length;i++)
           document.getElementById(objekty[medziobjekt].nazvy[i]).style.display = "none";
-        }
-
-
-
-      }
-      
-      
-
-      function hubyFunction() {
-        // Get the checkbox
-        // Get the output text
-        // If the checkbox is checked, display the output text
-        if (checkBoxHuby.checked == true){
-          //galeria.style.display = "grid";
-          for(i=0;i<huby.length;i++){
-              document.getElementById(hubyNazvy[i]).style.display = "block";
-          }
-        } else {
-          //galeria.style.display = "none";
-          for(i=0;i<huby.length;i++){
-            document.getElementById(hubyNazvy[i]).style.display = "none";
-        }
-        }
-      }
-
-      function monocotFunction() {
-        // Get the checkbox
-        // Get the output text
-        // If the checkbox is checked, display the output text
-        if (checkboxMonocot.checked == true){
-          //galeria.style.display = "grid";
-          for(i=0;i<monocot.length;i++){
-              document.getElementById(monocotNazvy[i]).style.display = "block";
-          }
-        } else {
-          //galeria.style.display = "none";
-          for(i=0;i<monocot.length;i++){
-            document.getElementById(monocotNazvy[i]).style.display = "none";
-        }
-        }
-      }
-
-      function dicotFunction() {
-        // Get the checkbox
-        // Get the output text
-        // If the checkbox is checked, display the output text
-        if (checkboxDicot.checked == true){
-          //galeria.style.display = "grid";
-          for(i=0;i<dicot.length;i++){
-              document.getElementById(dicotNazvy[i]).style.display = "block";
-          }
-        } else {
-          //galeria.style.display = "none";
-          for(i=0;i<dicot.length;i++){
-            document.getElementById(dicotNazvy[i]).style.display = "none";
-        }
         }
       }
       
