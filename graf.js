dd981577ce02cfae81ba6f309a4629586c5b1e16
jozoforza <s.macica7@gmail.com>
2022-05-26 16:06:30 +0200

    graf animation

--- graf.js
+++ graf.js
@@ -1,27 +1,44 @@
 
 const hubygraf={
     pocet: 3,
+    name: "huby/lyšajníky",
+    graf: 0,
 }
 const riasygraf={
     pocet: 4,
+    name: "riasy",
+    graf: 0,
+    
 }
 const machograf={
     pocet: 2,
+    name: "machorasty",
+    graf: 0,
 }
 const papragraf={
     pocet: 1,
+    name: "papraďorasty",
+    graf: 0,
 }
 const nahograf={
     pocet: 5,
+    name: "nahosemenné",
+    graf: 0,
 }
 const monograf={
     pocet: 6,
+    name: "jednoklíčnolisté",
+    graf: 0,
 }
 const digraf={
     pocet: 7,
+    name: "dvojklíčnolisté",
+    graf: 0,
 }
 const specialgraf={
     pocet: 3,
+    name: "špecialne",
+    graf: 0,
 }
 const stlpce=[hubygraf,riasygraf,machograf,papragraf,nahograf,monograf,digraf,specialgraf];
 
@@ -33,11 +50,11 @@ for(i=0;i<stlpce.length;i++){
     graf.innerHTML+=`
         <div class="cellTable">
 
-        <div class="grafText"> <p>Machy</p> </div>
+        <div class="grafText"> <p>${stlpce[i].name}(${stlpce[i].pocet})</p> </div>
 
-        <div  style="width: 100%;
+        <div id="${stlpce[i].name}" style="width: 100%;
             display: grid;
-            grid-template-columns: ${g+"%"} auto;
+            grid-template-columns: ${0+"%"} auto;
             margin-right: 8px;">
             <div class="stlpec" style="background: rgb(${i*10+","+220+","+i*30});"></div>
             <div class="Prazdno"></div>
@@ -45,5 +62,37 @@ for(i=0;i<stlpce.length;i++){
     `
     
 }
+
+//document.getElementById(stlpce[1].name).style.gridTemplateColumns = "50% auto"; 
+/*for(i=0;i<stlpce.length;i++){
+    let g=Math.round((stlpce[i].pocet/7)*100)
+
+}*/
+stepTime=20;
+let krok=0;
+dlzka = 0;
+
+function frame() {
+    if (dlzka == 100) {
+    clearInterval(id);
+    } else {
+        dlzka++; 
+        
+        for(i=0;i<stlpce.length;i++){
+
+                let krok=(Math.round((stlpce[i].pocet/7)*100))/100
+                document.getElementById(stlpce[i].name).style.gridTemplateColumns = krok+stlpce[i].graf+"% auto";
+                stlpce[i].graf=stlpce[i].graf+krok
+            
+    }
+    }
+}
+
+    
+    id = setInterval(frame, stepTime);
+    
+
+ 
+
 
 
